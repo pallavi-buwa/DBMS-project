f9154af9b355f855d659a7dc38d92ed1ef86f102
Archisha Shukla <archishashukla@gmail.com>
2021-11-02 12:30:24 +0530

    Add : Script for searching --- still have to fix issues

--- User/bookSlot.php
+++ User/bookSlot.php
@@ -13,7 +13,6 @@
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
         
         <link rel="stylesheet" href="../style.css">
-        
 
     </head>
     <body>
@@ -45,20 +44,11 @@
             <form class="navbar-search">
                 <center>
                     <ul class="nav navbar-nav">
-                      <li><input type="text" class="search-query" placeholder="Search here" />  </li>      
-                      <li><label for="mySubmit" class="btn-small"><h2><i class="fas fa-search"></h2></i></label></li>
-                      <li><input id="mySubmit" type="submit" value="Go" class="hidden" /></li>
-                      <li>
-                        <div class="dropdown">
-                            <button id ="dropdown"class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Sort
-                            <span class="caret"></span></button>
-                            <ul class="dropdown-menu">
-                              <li><a href="#" id="cap_id">Capacity</a></li>
-                              <li><a href="#" id="hosp_id">Hospital Name</a></li>
-                              <li><a href="#" id="pin_id">Pincode</a></li>
-                            </ul>
-                          </div>
-                      </li>
+                       <form action="" method="post">
+                        <input type="text" name="search">
+                        <input type="submit" name="submit" value="Search">
+                        </form>
+
                       </ul>
                   </center>
             </form>
@@ -78,33 +68,36 @@
               </tr>
             </thead>
             <tbody>
-             <?php
-              $servername = "localhost";
-              $username = "root";
-              $password = "";
-              $dbname = "vaccine_records";
-
-               $con = new mysqli($servername, $username, $password, $dbname);
-                if (!$con){
-                  die('Could not connect: ' . mysql_error());
-                 }
-                
-                $query ="SELECT hospital.h_id,hospital.h_name,hospital.pin,hospital.address,hospital_vacc.time_slots,hospital_vacc.capacity,vaccines.type FROM hospital JOIN hospital_vacc ON hospital.h_id=hospital_vacc.h_id JOIN vaccines ON vaccines.vac_id=hospital_vacc.vac_id;";
-                $result = $con->query($query);
-                while($row = $result->fetch_assoc()) {
-                  echo "<tr>";
-                  echo "<td>" . $row['h_id'] . "</td>";
-                  echo "<td>" . $row['h_name'] . "</td>";
-                  echo "<td>" . $row['pin'] . "</td>";
-                  echo "<td>" . $row['address'] . "</td>";
-                  echo "<td>" . $row['time_slots'] . "</td>";
-                  echo "<td>" . $row['capacity'] . "</td>";
-                  echo "<td>" . $row['type'] . "</td>";
-                  echo "</tr>";
+            <?php
+                         $search_value=filter_input(INPUT_POST,'search');
+                         print($search_value);
+                         echo $search_value;
+                         $servern = "localhost";
+                         $usern = "root";
+                         $passw = "";
+                         $dbn = "vaccine_records";
+                         $conn=new mysqli($servern,$usern,$passw,$dbn);
+                        if($conn->connect_error){
+                            echo 'Connection Faild: '.$con->connect_error;
+                            }else{
+                                $sql = "SELECT hospital.h_id,hospital.h_name,hospital.pin,hospital.address,hospital_vacc.time_slots,hospital_vacc.capacity,vaccines.type FROM hospital JOIN hospital_vacc ON hospital.h_id=hospital_vacc.h_id JOIN vaccines ON vaccines.vac_id=hospital_vacc.vac_id where hospital.h_name like '%$search_value%'or hospital.address like '%$search_value%' or vaccines.type like '%$search_value%';";
+                                $res= $conn->query($sql) or die($conn->error);
+                                while($row = $res->fetch_assoc()) {
+                                echo "<tr>";
+                                echo "<td>" . $row['h_id'] . "</td>";
+                                echo "<td>" . $row['h_name'] . "</td>";
+                                echo "<td>" . $row['pin'] . "</td>";
+                                echo "<td>" . $row['address'] . "</td>";
+                                echo "<td>" . $row['time_slots'] . "</td>";
+                                echo "<td>" . $row['capacity'] . "</td>";
+                                echo "<td>" . $row['type'] . "</td>";
+                                echo "</tr>";
 
                   }
 
-                $con->close();?>
+                                }
+                                $conn->close();
+                                ?>
             </tbody>
           </table>
           <br>
